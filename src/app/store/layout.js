365a6bb5a6b1fcee17fd941676fcc4eae2ef538b
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-11-09 06:29:21 +0600

    layout created under store route

---
+++ src/app/store/layout.js
@@ -0,0 +1,15 @@
+import Sidebar from "../ui/sidebar/Sidebar";
+
+export const metadata = {
+  title: "Book Bridge - Buy or Rent Books",
+  description: "A Online Book Store",
+};
+
+export default function BooksLayout({ children }) {
+  return (
+    <div className="flex xs:flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row min-h-screen">
+      <Sidebar />
+      <div className="flex flex-wrap p-2 justify-center">{children}</div>
+    </div>
+  );
+}
