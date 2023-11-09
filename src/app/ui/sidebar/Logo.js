134453a70f739ba16c88112c4fc8c2529dbc4d9f
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-11-09 06:35:35 +0600

    logo added in the navbar

---
+++ src/app/ui/sidebar/Logo.js
@@ -0,0 +1,12 @@
+import { BookOpenIcon } from "@heroicons/react/24/outline";
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div>
+      <BookOpenIcon className="h-12 w-12" />
+    </div>
+  );
+};
+
+export default Logo;
--- src/app/ui/sidebar/Sidebar.js
+++ src/app/ui/sidebar/Sidebar.js
@@ -1,1 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
+const Sidebar = () => {
+  return (
+    <div>
+      <Link href="/store">
+        <Logo />
+      </Link>
+      <div>
+        <NavLinks />
+
+        <form>
+          <button>Sign Out</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
